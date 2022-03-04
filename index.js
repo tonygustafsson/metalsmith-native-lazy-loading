051d6fa86e3bae04ae3a1c44a0a6126973be5db6
depuits <colmanjoeri@gmail.com>
2022-03-04 10:28:30 +0100

    made css selector an option

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function plugin(opts) {
                 var $ = cheerio.load(data.contents.toString());
 
                 // Find all images in articles that is missing loading attribute
-                var $imagesWithoutLoadingDefined = $('.article__content img:not([loading])');
+                var $imagesWithoutLoadingDefined = $(opts.selector + ' img:not([loading])');
 
                 // Add loading lazy to all images
                 $imagesWithoutLoadingDefined.attr('loading', 'lazy');
